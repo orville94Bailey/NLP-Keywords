94130e849b31301817b264e58263e7ef69cf9429
michael <macbad2012@gmail.com>
2017-10-15 15:52:25 -0400

    Eliminates all unicode codes now. Will also have increased accuracy if a title can be provided. It will work just as before if unable to do so though. Third parameter is that optional title (It's meant to be a single string with words seperated by whitespace in some way).

--- Main.py
+++ Main.py
@@ -4,23 +4,24 @@ from rake_nltk import Rake
 # parameters are as follows:
 # text (string: text to obtain keywords from)
 # numOfKeywords (integer: how many keywords to obtain from text)
-def getKeywords(text, numOfKeywords):
+# title (string: the title of the text if applicable(if left empty it won't be used))
+def getKeywords(text, numOfKeywords, title=""):
 
     # remove words which are not valid keywords or are not useful
     # parameters are as follows:
     # words (list of strings: words to scrub of junk data and irrelevant data)
     def scrubList(words):
 
-        invalidWords = [u'\u201d', u'\u201c', u'\u2019', u'\u2018', u'\u2014']
+        invalidWords = []
 
         i = 0
         while (i < len(words)):
-            # if the word is invalid, remove it (don't increment iteration to stay in range)
-            if (words[i] in invalidWords):
+            # remove everything that's invalid
+            # currently it will remove: unicode codes, specified strings in the invalidWords list
+            if ('\u' in words[i].encode('raw_unicode_escape') or words[i] in invalidWords):
                 del words[i]
             else:  # otherwise increment iteration
                 i += 1
-
         return words
 
     # clean keywords; some keywords from rake have junk in them
@@ -38,6 +39,20 @@ def getKeywords(text, numOfKeywords):
     rak.extract_keywords_from_text(text)
     wordDic = rak.get_word_degrees()
 
+    # use the title if relevant
+    if(title != ""):
+        # make it a list by splitting on whitespace
+        titleWords = title.split()
+
+        # lower everything for accuracy
+        for it in range(0, len(titleWords)):
+            titleWords[it] = titleWords[it].lower()
+
+        # if a keyword was in the title, double it's weight because it's likely very relevant
+        for word in wordDic:
+            if(word in titleWords):
+                wordDic[word] = wordDic[word]*2
+
     rankedWords = sorted(wordDic, key=wordDic.get, reverse=True)
     rankedWords = scrubList(rankedWords)
     returnDic = {}
@@ -49,7 +64,7 @@ def getKeywords(text, numOfKeywords):
         temp = rankedWords[it]
         temp = scrubWord(temp) # scrub the word to be stored without changing it's value in the list
 
-        #add the new, scrubbed word and it's weight
+        # add the new, scrubbed word and it's weight
         returnDic[temp.encode('UTF8')] = (wordDic[rankedWords[it]] * (1.0/len(wordDic)))
 
     return returnDic
@@ -63,3 +78,4 @@ def getKeywords(text, numOfKeywords):
 #art.parse()
 
 #print(getKeywords(art.text, 10))
+#print(getKeywords(art.text, 10, art.title))
